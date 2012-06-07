25a846efad0bc2a36fabf012937e33d17e511d17
Michael Brown <michaelb@arisechurch.org.nz>
2012-06-07 22:49:24 +1200

    Installed rails, added google_visualr and twitter-bootstrap-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerStatus::Application.config.secret_token = 'e2a52726c6645ece1e89bece5ad26066d41841884f28d76362b3afcd2532a628504ebb95a9e26b50fdf4c4e699e0ee4660f696bdb69711881a90f761bea3a21c'
